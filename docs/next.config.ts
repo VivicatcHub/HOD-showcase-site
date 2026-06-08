cea6aa9b86fe3ba0e2c862ea3d2cb77c82a38c66
Vivicaty <valentin.guinet@epitech.eu>
2026-06-08 16:29:58 +0200

    [MOV] for github pages again

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/HOD-showcase-site",
+  trailingSlash: true,
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
